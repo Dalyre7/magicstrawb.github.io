7aa7c7e155540896e66da30f9d37f88f6c5b22c2
Dalyre7 <110495237+Dalyre7@users.noreply.github.com>
2023-05-23 13:59:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("btn").onclick = function() {
+    let text = document.getElementById("text").value;
+    window.alert("UH OH. You lost. No stronklebry for u :(")
+    console.log(text)
+}
